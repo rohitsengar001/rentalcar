7d4cb774ba9c8e93c7064a7d8df52c16e2c1a717
rohitsengar001 <rohitsengar001@gmail.com>
2022-08-04 19:27:08 +0530

    Registration Api:💉 improve

--- api/agency/agency_registration.php
+++ api/agency/agency_registration.php
@@ -15,17 +15,26 @@ class agency_registration
     private $password;
     private $user_type;
     private $mobile_number;
-    private $address;
     private $conn;
+    private $address1;
+    private $address2;
+    private $city;
+    private $country;
+    private $zipcode;
 
-    function __construct($username, $password, $mobile_number, $address)
+    function __construct($username, $password, $mobile_number, $address1, $address2, $city, $country, $zipcode)
     {
         $this->username = $username;
         $this->password = $password;
         $this->user_type = 'agency';
         $this->mobile_number = $mobile_number;
-        $this->address = $address;
+        $this->address1 = $address1;
+        $this->address2 = $address2;
+        $this->city = $city;
+        $this->country = $country;
+        $this->zipcode = $zipcode;
         $this->conn = DbConnection::connect();
+
     }
 
 
@@ -40,13 +49,13 @@ class agency_registration
         elseif (!isset($this->username)
             || !isset($this->password)
             || !isset($this->mobile_number)
-            || !isset($this->address)
+            || !isset($this->address1)
             || empty(trim($this->username))
             || empty(trim($this->password))
             || empty(trim($this->mobile_number))
-            || empty(trim($this->address))
+            || empty(trim($this->address1))
         ) {
-            $fields = ['fields' => ['email', 'password']];
+            $fields = ['fields' => ['email', 'password','address1','phone']];
             $return_data = $this->msg(0, 422, 'Please Fill in all Required Fields!', $fields);
         } //CHECK VALID FORMAT FOR EMAIL
         elseif (!$this->filter_email(trim($this->username))) {
@@ -54,6 +63,9 @@ class agency_registration
         } //CHECK VALID FORMAT FOR PASSWORD
         elseif (!$this->filter_pass(trim($this->password))) {
             $return_data = $this->msg(0, 422, 'Invalid Password!');
+        } //VALIDATE MOBILE NUMBER
+        elseif (!$this->filter_mobile(trim($this->mobile_number))) {
+            $return_data = $this->msg(0, 422, 'Invalid Mobile Number!');
         } //EMAIL ALREADY EXIST IN DATABASE
         elseif ($this->is_email_exist()) {
             $return_data = $this->msg(0, 422, 'Email Address Already Registered!');
@@ -85,6 +97,11 @@ class agency_registration
         return !!preg_match_all("$\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])(?=\S*[\W])\S*$", $password);
     }
 
+    private function filter_mobile($phone)
+    {
+        return !!preg_match('/^[0-9]{10}+$/', $phone);
+    }
+
     private function is_email_exist(): bool
     {
         $email = $this->username;
@@ -99,7 +116,8 @@ class agency_registration
 
 $data_receive = json_decode(file_get_contents("php://input"));
 $agency_registration = new agency_registration($data_receive->username, $data_receive->password,
-    $data_receive->mobile_number, $data_receive->address);
+    $data_receive->mobile_number, $data_receive->address1, $data_receive->address2,
+    $data_receive->city, $data_receive->country, $data_receive->zipcode,);
 
 echo json_encode($agency_registration->postdata());
 
