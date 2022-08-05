216de29fa6137b761aec4a3bb974eb2e6eaa06c5
rohitsengar001 <rohitsengar001@gmail.com>
2022-08-06 00:20:25 +0530

    🎊🎊 Add : agency-component

--- modules/agency/agency-component.php
+++ modules/agency/agency-component.php
@@ -1,39 +1,157 @@
 <?php
+
 namespace rental\modules\agency;
 session_start();
 define("BLOCK_DIRECT_ACCESS", true);
+
 class agency_component
 {
     function __construct()
     {
         if (isset($_SESSION['username'])) {
-            echo "session:".$_SESSION['username'];
-            include  './agency-view.php';
+//            echo "session:".$_SESSION['username'];
+            include './agency-view.php';
         } else {
 
             echo "you're not valid user";
             header("Refresh: 5;url= http://localhost/rentalcar/modules/auth/login-component");
         }
     }
-    public function logout()
+
+    /**
+     * ## SUPPORT EXTENSION: ['jpg', 'png', 'jpeg']
+     * @param $file_name
+     * @return bool
+     */
+    public function filter_file($file_name): bool
     {
-        echo "logout-calling";
-        unset($_SESSION['username']);
-        session_destroy();
-        exit();
-//        header("Location: http://localhost/rentalcar/modules/auth/login-component");
+        $file_stored = ['jpg', 'png', 'jpeg'];
+        $split_file = explode('.', $file_name);
+        $check_extension = strtolower(end($split_file));
+        if (in_array($check_extension, $file_stored))
+            return true;
+        return false;
+    }
+
+
+    /**
+     * @param $data
+     * @return void
+     */
+    public function post_data_api($data)
+    {
+
+        $curl = curl_init();
+        $post_data = json_encode($data);
+        curl_setopt_array($curl, array(
+            CURLOPT_PORT => "80",
+            CURLOPT_URL => "http://localhost:80/rentalcar/api/agency/postcar_api.php",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => $post_data,
+            CURLOPT_HTTPHEADER => array(
+                "cache-control: no-cache",
+                "content-type: application/json",
+                "postman-token: 316128d9-4dea-4235-2daa-d8639a550644"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            //JSON TO PHP OBJECT
+            $res = json_decode($response);
+            echo "<script>console.log('.$res->message.')</script>";
+            echo "<script>console.log('post-btn clicked')</script>";
+            $this->alert_message($res);
+        }
+    }
+
+    public function alert_message($data)
+    {
+        if (!$data->success) {
+            echo '
+        <script>
+            let alertDiv = document.getElementById("alert-div");
+            let child_alert_div=`<strong>"' . $data->message . '"</strong> You should check in on some of those fields below.
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close" onclick="closeAlertDiv()">
+                                        <span aria-hidden="true">&times;</span>
+                                         </button>`;
+                    alertDiv.innerHTML=child_alert_div;
+                    alertDiv.style.display = "block";
+           
+        </script>
+        ';
+
+        }
+        echo "<script>
+            setTimeout(()=>{
+                window.location = 'http://localhost/rentalcar/modules/agency/agency-component.php';
+             },1000)
+            </script>";
+    }
+
+    public function msg_console($msg)
+    {
+        echo "<script>console.log('.$msg.')</script>";
     }
 }
 
+//INSTANTIATING THE OBJECT
 $agency_component = new agency_component();
 
-?>
+//GET PARAMETERS FROM THE VIEW
+//WHILE USER CLICK ON SUBMIT BUTTON
+if (isset($_POST['post-btn'])) {
+    if (!empty($_POST['vehicle-modal']) && !empty($_POST['vehicle-number']) && !empty($_POST['seating-capacity'])
+        && !empty($_POST['rent-per-day']) && !empty($_FILES['car-image'])
+    ) {
+        // GET VALUE OF FORMS FIELDS
+        $vehicle_model = $_POST['vehicle-modal'];
+        $vehicle_number = $_POST['vehicle-number'];
+        $seating_capacity = $_POST['seating-capacity'];
+        $rent_per_day = $_POST['rent-per-day'];
+        $file = $_FILES['car-image'];
+        $file_name = $file['name'];
+        $temp_file_name = $file['tmp_name'];
+        $file_error = $file['error'];
+        $username = "test@gmail.com";
+        $token = "valid-user";
+        $image_destination = 'C:/xampp/htdocs/rentalcar/upload/' . $file_name;
 
-<script>
-    let logoutBtn = document.getElementById("logout-btn");
-    function logout(){
-        console.log("logout : from js");
-        //let msg ="<?php //$agency_component->logout();?>//";
+        //SET PAYLOAD FOR CALL POST API
+        $payload = array(
+            "username" => $username,
+            "vehicle_model" => $vehicle_model,
+            "vehicle_number" => $vehicle_number,
+            "seating_capacity" => $seating_capacity,
+            "rent_per_day" => $rent_per_day,
+            "file_name" => $file_name,
+            "temp_file_name" => $temp_file_name,
+            "image_destination" => $image_destination,
+            "token" => $token
+        );
+//        print_r($payload);
+        //VALIDATE FILE EXTENSION
+        if ($agency_component->filter_file($file_name)) {
+            move_uploaded_file($temp_file_name, $image_destination);
+//            echo "move_upload_file : success";
+            $agency_component->post_data_api($payload);
+        }
+    } else {
+        echo "Request Failed";
     }
-</script>
+}
+
+
+
 
--- modules/agency/agency-view.php
+++ modules/agency/agency-view.php
@@ -1,31 +1,182 @@
 <?php
- if(!defined('BLOCK_DIRECT_ACCESS')){
-     header("Location: ../../../notfound");
- }
+if (!defined('BLOCK_DIRECT_ACCESS')) {
+    header("Location: ../../../notfound");
+}
 ?>
-  <!doctype html>
-      <html lang="en">
-  <head>
-  <meta charset="UTF-8">
-               <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-                           <meta http-equiv="X-UA-Compatible" content="ie=edge">
-               <title>agency_dashboard</title>
-      <!-- CSS only -->
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-  </head>
-  <body>
-  <!-- JavaScript Bundle with Popper -->
-  <div class="alert alert-success" role="alert">
-      <h4 class="alert-heading">Welcome dashboard!</h4>
-      <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
-      <hr>
-      <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
-  </div>
+<!doctype html>
+<html lang="eng">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>agency_dashboard</title>
+    <!-- CSS only -->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+          integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
-      <a href="./../auth/logout.php" class="btn btn-danger">Logout</a>
+</head>
+<body>
+<!-- JavaScript Bundle with Popper -->
+<div class="container-fluid">
+    <!--      NAVBAR-->
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #b7d02d;">
+        <!-- Navbar content -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+                aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#"><span class="text-danger font-weight-bold shadow-sm">CarRental</span></a>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="../login-component.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                <a href="./../auth/logout.php" class="btn btn-danger m-2">Logout</a>
+            </form>
+        </div>
+    </nav>
+    <!-- NAVBAR END-->
+    <div class="alert bg-dark text-light" role="alert">
+        <h4 class="alert-heading">Welcome dashboard!</h4>
+        <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer
+            so that you can see how spacing within an alert works with this kind of content.</p>
+        <hr>
+        <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+    </div>
+
+</div>
+<div class="container">
+    <!--    ALERT-DIV-->
+    <div class="alert alert-danger alert-dismissible fade show" role="alert" id="alert-div" style="display:none " >
+        <!--  SHOW ERROR MESSAGE !-->
+    </div>
+    <!-- ALERT DIV END   -->
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body bg-light shadow-lg">
+                    <h5 class="card-title">Post Your Vehicles <i class="bi bi-car-front-fill"></i></h5>
+                    <p class="card-text"><span class="badge badge-success">Rent</span> Your Vehicles in just a second
+                    </p>
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#postModal"
+                            data-whatever="@fat">Post Vehicles
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body bg-light shadow-lg">
+                    <h5 class="card-title">Check Your Booking <i class="bi bi-airplane-engines-fill"></i></h5>
+                    <p class="card-text">Easily Track your Booking.</p>
+                    <a href="#" class="btn btn-primary">Show Booking</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--   POST-VEHICLES MODAL-->
+    <div class="modal fade" id="postModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+
+                    <h5 class="modal-title" id="exampleModalLabel">POST YOUR VEHICLES</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="<?php echo ($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data" class="was-validated">
+
+                        <div class="form-group">
+                            <label for="vehicleModal" class="col-form-label">Vehicle Modal</label>
+                            <input type="text" class="form-control text-uppercase" id="vehicleModal" name="vehicle-modal" required minlength="3" maxlength="40" >
+                            <div class="invalid-feedback">
+                               please write Correct Model Name
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="vehicleNumber" class="col-form-label">Vehicle Number</label>
+                            <input type="text" class="form-control text-uppercase" id="vehicleNumber" name="vehicle-number" required minlength="7" maxlength="8"></input>
+                            <div class="invalid-feedback">
+                                Required Min Length:7 , Max:8
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="rentPerDay" class="col-form-label">Rent Per Day</label>
+                            <input type="number" class="form-control" id="rentPerDay" name="rent-per-day" required min="100" ></input>
+                            <div class="invalid-feedback">
+                                required!
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <label class="input-group-text" for="seatingCapacity">Seating Capacity</label>
+                            </div>
+
+                            <select class="custom-select" id="seatingCapacity" name="seating-capacity" required>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                                <option value="4">Four</option>
+                                <option value="6">Five</option>
+                                <option value="7">Six</option>
+                            </select>
+                            <div class="invalid-feedback">
+                                Required!
+                            </div>
+                        </div>
+
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <button class="btn btn-outline-secondary" type="button" id="inputGroupFileAddon03" data-toggle="tooltip" data-placement="top" title="jpeg,jpg and png format only">
+                                    Upload Vehicle Image <i class="bi bi-info-circle-fill"></i>
+                                </button>
+                            </div>
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" id="carImage"
+                                       aria-describedby="inputGroupFileAddon03" name="car-image" required>
+                                <label class="custom-file-label" for="inputGroupFile03">Choose file</label>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+<!--                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>-->
+                            <button  class="btn btn-primary" name="post-btn">Post</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--  POST-VEHICLES  MODAL END-->
+</div>
 
 
-  </body>
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
-     <script src="agency.js"></script>
-  </html>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"
+        integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+"
+        crossorigin="anonymous"></script>
+</body>
+<script src="agency.js"></script>
+
+</html>
