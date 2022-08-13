7b6a4a36aa1deb454e72daa0d8f83999868e4be1
rohitsengar001 <rohitsengar001@gmail.com>
2022-08-13 11:59:08 +0530

    🪄🍧 booking_api done

--- api/customer/booking_api.php
+++ api/customer/booking_api.php
@@ -1,11 +1,83 @@
 <?php
 
 namespace rentalcar\api\customer;
+require_once '../common/DbConnection.php';
+require_once '../common/api_utility.php';
 
 use api\common\api_utility;
+use mysqli;
+use rentalcar\api\common\DbConnection;
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header("Content-Type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Headers:Content-Type,Access-Control-Allow-Method,Access-Control-Allow-Origin');
 
 class booking_api extends api_utility
 {
+    private $vehicle_id;
+    private $customer_id;
+    private $days;
+    private $booking_date;
+    private $billing_amount;
+    private mysqli $conn;
 
+    /**
+     * @param $vehicle_id
+     * @param $customer_id
+     * @param $booking_date
+     * @param $days
+     * @param $billing_amount
+     */
+    public function __construct($vehicle_id, $customer_id, $booking_date, $days, $billing_amount)
+    {
+        $this->vehicle_id = $vehicle_id;
+        $this->customer_id = $customer_id;
+        $this->booking_date = $booking_date;
+        $this->days = $days;
+        $this->billing_amount = $billing_amount;
+        $this->conn = DbConnection::connect();
+    }
+
+
+    public function booked_vehicle(): array
+    {
+        $return_data = [];
+
+        //CHECK ANY ONE FIELDS HAVE EMPTY AND NULL VALUE
+        if ((!isset($this->vehicle_id))
+            || (!isset($this->customer_id))
+            || (!isset($this->booking_date))
+            || (!isset($this->days))
+            || (!isset($this->billing_amount))
+            || (empty(trim($this->vehicle_id)))
+            || (empty(trim($this->customer_id)))
+            || (empty(trim($this->booking_date)))
+            || (empty(trim($this->days)))
+            || (empty(trim($this->billing_amount)))
+        ) {
+            $fields = ["vehicle_id", "customer_id", "days", "booking_date", "billing_amount"];
+            $return_data = $this->msg(0, 404, "KINDLY FILL ALL MANDATORY FIELDS", $fields);
+        } // VALIDATION OF CUSTOMER ID
+        elseif (!$this->is_user_exist($this->conn, "auth_customer", $this->customer_id)) {
+            $return_data = $this->msg(0, 404, "CUSTOMER IS NOT EXIST!");
+        } //VALIDATE VEHICLE ID
+        elseif (!$this->is_vehicle_exist($this->conn, $this->vehicle_id)) {
+            $return_data = $this->msg(0, 404, 'VEHICLE IS NOT EXIST!');
+        } else {
+            $query = $this->conn->prepare("INSERT INTO booking(customer_id, vehicle_id, days,booking_date, billing_amount) VALUES(?,?,?,?,?)");
+            $query->bind_param('sssss', $this->customer_id, $this->vehicle_id, $this->days, $this->booking_date, $this->billing_amount);
+            if ($query->execute()) {
+                $return_data = $this->msg(1, 200, "VEHICLE SUCCESSFULLY BOOKED!");
+            } else {
+                $return_data = $this->msg(1, 500, "MYSQL INTERNAL ERROR");
+            }
+        }
+        return $return_data;
+    }
 
 }
+
+$receive_data = json_decode(file_get_contents("php://input"));
+$comp = new booking_api($receive_data->vehicle_id, $receive_data->customer_id, $receive_data->booking_date, $receive_data->days, $receive_data->billing_amount);
+echo json_encode($comp->booked_vehicle());
