dd2caf8f66ca5f74e39e7db976eb3ed43b2ea9aa
rohitsengar001 <rohitsengar001@gmail.com>
2022-08-17 14:36:04 +0530

    Remove Dead code

--- modules/common/navbar.php
+++ modules/common/navbar.php
@@ -1,28 +1,22 @@
 <!--      NAVBAR-->
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #b7d02d;">
-        <!-- Navbar content -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
-                aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="navbar-brand" href="#"><span class="text-danger font-weight-bold shadow-sm">CarRental</span></a>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #b7d02d;">
+    <!-- Navbar content -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"
+            aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#"><span class="text-danger font-weight-bold shadow-sm">CarRental</span></a>
 
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item active">
-                    <a class="nav-link" href="./agency-component">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                <a href="./../auth/logout.php" class="btn btn-danger m-2">Logout</a>
-            </form>
-        </div>
-    </nav>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+                <a class="nav-link" href="./agency-component">Home <span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <a href="./../auth/logout.php" class="btn btn-danger m-2">Logout</a>
+        </form>
+    </div>
+</nav>
